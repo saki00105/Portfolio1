6dd9ebc32131931a49c611f5e0bbeacc0fd15817
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-09-19 20:21:46 +0900

    file

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="format-detection" content="telephone=no">
+        <meta name="description" content="WordPress theme development static data for beginners">
+        <meta name="keywords" content="WordPress, Theme, development">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/images/common/ico/favicon.ico">
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <header class="header">
+            <h1 class="header__ttl"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></h1>
+           
+            <?php wp_nav_menu( array(
+                'theme_location' => 'header-menu',
+                ) );
+            ?>
+        </header>
+    
+    </body>
+</html>
---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+    <div class="c-wrap">
+        <div class="c-grid">
+            <div class="p-contents">
+                
+                <?php the_content(); ?>
+                                
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+<?php get_footer(); ?>
