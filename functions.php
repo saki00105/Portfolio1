0fbc4a914824a01914416a5db4323e41f55911f9
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-10-05 23:31:01 +0900

    font

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@ add_theme_support( 'automatic-feed-links' );
 
 //スタイルシート
 function wpbeg_script() {
-    wp_enqueue_style( 'Roboto','https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap' , array() );
-    wp_enqueue_style( 'Mplus1', 'https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@400;700&display=swap', array() );
+    wp_enqueue_style( 'Montserrat','https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@400;700&family=Montserrat:wght@400;700;900&display=swap' , array() );
+    wp_enqueue_style( 'Rounded+1','https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c' , array() );
     wp_enqueue_style( 'reset', get_theme_file_uri( '/css/reset.css' ), array(), '1.0.0');
     wp_enqueue_style( 'app', get_theme_file_uri( '/css/app.css' ) , array(), '1.0.0');
     wp_enqueue_style( 'style', get_theme_file_uri( '/css/style.css' ) , array(), '1.0.0');
@@ -36,32 +36,8 @@ add_action('wp_print_scripts', 'custom_print_scripts');
 
 
 //エディタスタイルの追加
-function wpbeg_theme_add_editor_styles() {
-  wp_enqueue_style( 'editor-style', get_theme_file_uri( '/css/editor-style.css' ) , array(), '1.0.0');
-}
-add_action( 'admin_init', 'wpbeg_theme_add_editor_styles' );
+// function wpbeg_theme_add_editor_styles() {
+//   wp_enqueue_style( 'editor-style', get_theme_file_uri( '/css/editor-style.css' ) , array(), '1.0.0');
+// }
+// add_action( 'admin_init', 'wpbeg_theme_add_editor_styles' );
 
-//カスタムメニュー
-function theme_setup(){
-  register_nav_menus( 
-    array(
-      'header-menu' => 'ヘッダーメニュー',
-      'footer-menu' => 'フッターメニュー',
-    )
-  );
-}
-add_action( 'after_setup_theme', 'theme_setup');
-
-
-
-//WordPressの処理で現在のユーザーがセットされたら実行
-add_action('set_current_user', function (){
-
-	if(current_user_can('administrator')){ //管理者ユーザーなら
-		$_SESSION["_wp_debug"] = 1; //セッション内にデバッグ有効フラグを保存
-	}else{
-		$_SESSION["_wp_debug"] = 0; //セッション内にデバッグ無効フラグを保存
-	}
-	date_default_timezone_set( 'Asia/Tokyo' ); //日本時間の設定
-
-});
